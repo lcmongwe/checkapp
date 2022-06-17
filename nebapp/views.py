e66470a7aba845ff8abcf4ed12859407bfd8afea
lcmongwe <lcmongwe@gmail.com>
2022-06-17 07:57:48 +0300

    custom user  reation form created

--- nebapp/views.py
+++ nebapp/views.py
@@ -5,7 +5,16 @@ import datetime as dt
 from django.contrib import messages
 from .forms import *
 from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from django.conf import settings
 
+
+# authentication
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from .forms import RegisterUserForm
 
 # @login_required(login_url='login')
 def home(request):
