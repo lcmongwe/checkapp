9c7e3e44314aba510be4e3ecee65309ca8263330
lcmongwe <lcmongwe@gmail.com>
2022-06-17 07:54:38 +0300

    authentication views created

--- nebapp/views.py
+++ nebapp/views.py
@@ -11,3 +11,55 @@ from django.contrib.auth.decorators import login_required
 def home(request):
 
     return render(request, 'home.html', {})
+
+
+
+
+
+# AUTHENTICATION
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user= authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.success(request,('there ws an error loggig in, please try again...'))
+            return redirect('login')
+
+    else:
+        return render (request,'authenticate/login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,('you are logged out'))
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form =RegisterUserForm(request.POST)
+        if form.is_valid():
+
+            username=request.POST['username']
+            email=request.POST['email']
+            subject='welcome to InstaApp'
+            message=f'Hi {username} welcome to InstaApp and have fun! '
+            from_email=settings.EMAIL_HOST_USER
+            recipients=[email]
+            # send_mail(subject, message,from_email,recipients)
+
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request,('Regestration succesful'))
+            return redirect('home')
+    else:
+        form =RegisterUserForm()
+
+
+    return render(request,'authenticate/register.html',{'form':form,})
