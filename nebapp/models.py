045feba59a98578a76caeb7a20eab527d3d6f187
lcmongwe <lcmongwe@gmail.com>
2022-06-17 10:37:52 +0300

    authentication and login set up

--- nebapp/models.py
+++ nebapp/models.py
@@ -30,8 +30,8 @@ class User(AbstractUser):
     def delete_user(self):
         self.delete()
     
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
 
 class Business(models.Model):
     name = models.CharField(max_length=200,blank=True,null=True)
