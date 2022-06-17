20215812bc3c0445f275c8cddb0a4c9590353995
lcmongwe <lcmongwe@gmail.com>
2022-06-17 07:34:14 +0300

    static files configurations

--- nebprojcet/settings.py
+++ nebprojcet/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'nebapp',
+    'cloudinary_storage',
+
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                 'django.template.context_processors.media',
+
             ],
         },
     },
